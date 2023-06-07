0946bf3d75e21cce522dea17e70a910e4d76afed
julydeath <manojkarajada.mk@gmail.com>
2023-06-07 15:44:53 +0530

    chore : image slider in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai"
 import { useNavigate } from 'react-router-dom'
 
@@ -8,6 +8,7 @@ const Home = () => {
     const images = ["https://images.unsplash.com/photo-1603302576837-37561b2e2302?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1168&q=80", "https://images.unsplash.com/photo-1566150905458-1bf1fc113f0d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80", "https://images.unsplash.com/photo-1610792516307-ea5acd9c3b00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80", "https://images.unsplash.com/photo-1573148195900-7845dcb9b127?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1340&q=80", "https://images.unsplash.com/photo-1544468266-6a8948003cd7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1174&q=80"]
 
     const [count, setCount] = useState(0)
+    console.log(count)
 
     const handleLeft = () => {
         setCount((prev) => prev - 1)
@@ -22,6 +23,13 @@ const Home = () => {
             setCount(0)
         }
     }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            handleRight()
+        }, 1500)
+        return () => clearInterval(interval)
+    }, [count])
 
     return (
         <div className='my-4'>
